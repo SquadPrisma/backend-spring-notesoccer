68192d1c730e6b51e06463e6c579335d645c3845
leoacferraz <leoalvo89@gmail.com>
2025-07-31 17:50:24 -0300

    feat: estrutura inicial do projeto com profiles dev e prod

---
+++ src/main/java/com/squadprisma/notesoccer/NotesoccerApplication.java
@@ -0,0 +1,13 @@
+package com.squadprisma.notesoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesoccerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesoccerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squadprisma/notesoccer/NotesoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadprisma.notesoccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
